31250bb89312a6887c0893b61708cdbe7842bb53
Evžen Šupler <evzen.supler@gmail.com>
2021-11-24 12:14:06 +0100

    Update test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,6 +3,6 @@ import { expect } from 'chai'
 
 describe('helloWorld', () => {
     it('Should return greetings', () => {
-        expect(helloWorld()).equals('Howdy!')
+        expect(helloWorld()).equals("Hello from pakage!");
     })
 })
